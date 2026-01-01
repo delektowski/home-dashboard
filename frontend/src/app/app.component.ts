a741d58607979d4e6c428385251a59d83fb96579
Marcin Delektowski <delektowski@interia.pl>
2026-01-01 21:04:18 +0100

    feat: integrate global toggle button and implement update check functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {DarkModeService} from './services/dark-mode.service';
 import {HomeMeasuresService} from './services/home-measures.service';
-import { Button } from "primeng/button";
+import { GlobalToggleBtnComponent } from "./global-toggle-btn/global-toggle-btn.component";
+import { SwUpdate } from '@angular/service-worker';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, GlobalToggleBtnComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 
@@ -17,23 +18,34 @@ export class AppComponent implements OnInit {
 
   private darkModeService = inject(DarkModeService);
   protected homeMeasuresService = inject(HomeMeasuresService);
+  private swUpdate = inject(SwUpdate);
 
   ngOnInit() {
     this.toggleDarkMode();
+    this.checkForUpdates();
+  }
+
+  checkForUpdates() {
+    if (this.swUpdate.isEnabled) {
+      // Check for updates when app starts
+      this.swUpdate.versionUpdates.subscribe(event => {
+        if (event.type === 'VERSION_READY') {
+            window.location.reload();
+        }
+      });
+
+    }
   }
 
   toggleDarkMode() {
     const element = document.querySelector('html');
     element?.classList.toggle('app-dark-mode');
-
     this.darkModeService.toggleDarkMode(true);
 
   }
 
   toggleAllCollapsed() {
-
     this.homeMeasuresService.isAllCollapsed.set(!this.homeMeasuresService.isAllCollapsed());
-
   }
 }
 
--- frontend/src/app/global-toggle-btn/global-toggle-btn.component.ts
+++ frontend/src/app/global-toggle-btn/global-toggle-btn.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HomeMeasuresService } from '../services/home-measures.service';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-global-toggle-btn',
-  imports: [],
+  imports: [Button],
   templateUrl: './global-toggle-btn.component.html',
   styleUrl: './global-toggle-btn.component.scss'
 })
 export class GlobalToggleBtnComponent {
+  protected homeMeasuresService = inject(HomeMeasuresService);
+
+  toggleAllCollapsed() {
+    this.homeMeasuresService.isAllCollapsed.set(!this.homeMeasuresService.isAllCollapsed());
+  }
 
 }
