a491863838a2507d8a06043002f7a9dee2344d34
Marcin Delektowski <delektowski@interia.pl>
2026-01-11 22:30:39 +0100

    feat: add NoMeasuresComponent with initial template and styles

---
+++ frontend/src/app/home-measure/no-measures/no-measures.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-measures',
+  imports: [],
+  templateUrl: './no-measures.component.html',
+  styleUrl: './no-measures.component.scss'
+})
+export class NoMeasuresComponent {
+
+}
